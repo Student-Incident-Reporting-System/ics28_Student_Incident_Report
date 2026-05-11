de1f5ab7b1108964c93691ff83cc97a4cdfdc2fa
Lawrence-19 <lawrencesombreno@gmail.com>
2026-05-11 23:40:45 +0800

    updated logout.php

--- logout.php
+++ logout.php
@@ -6,23 +6,16 @@ require_once 'auth.php';
 require_once 'db.php';
 
 if (isLoggedIn()) {
-    logActivity($_SESSION['user_id'], 'LOGOUT', null, null, 'User logged out');
+    logActivity($_SESSION['user_id'], 'LOGOUT', null, null, 'Logged out');
 }
 
-// Clear all session data
 $_SESSION = [];
-
-// Destroy the session cookie
 if (ini_get('session.use_cookies')) {
-    $params = session_get_cookie_params();
-    setcookie(
-        session_name(), '', time() - 42000,
-        $params['path'], $params['domain'],
-        $params['secure'], $params['httponly']
-    );
+    $p = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000, $p['path'], $p['domain'], $p['secure'], $p['httponly']);
 }
-
 session_destroy();
-header('Location: index.php?logged_out=1');
+header('Location: index.php');
 exit();
+
 ?>
